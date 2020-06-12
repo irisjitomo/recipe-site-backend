cc3f673ce6ec786dd8da099aa58a2be2ddaca8a7
unknown <irisjitomo@yahoo.com>
2020-06-12 14:10:54 -0400

    cors

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const cors = require('cors');
 const helmet = require('helmet');
 require('dotenv').config()
 
+var corsOptions = {
+    origin: "*",
+    methods: ["POST", "GET", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}
 const authenticate = require('./auth/authenticate-middleWare');
 const authRouter = require('./auth/auth-Router');
 const savedRecipesRouter = require('./router/savedRecipes-router')
@@ -10,7 +16,7 @@ const savedRecipesRouter = require('./router/savedRecipes-router')
 const server = express();
 
 server.use(helmet());
-server.use(cors());
+server.use(cors(corsOptions));
 server.use(express.json());
 
 server.use('/api/auth', authRouter)
