15c6de492cfa4485e73c68d288021c973f0efe32
unknown <irisjitomo@yahoo.com>
2020-06-12 21:48:36 -0400

    added new route

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const server = express();
 // })
 
 server.use(helmet());
-server.use(express.json());
 server.use(cors())
+server.use(express.json());
 // server.options(cors(corsOptions))
 // server.use(cors(corsOptions));
 
