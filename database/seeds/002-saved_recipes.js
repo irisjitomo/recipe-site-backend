c2370b245b484acebe03a4af1f68c1883684a245
unknown <irisjitomo@yahoo.com>
2020-06-14 20:08:42 -0400

    more progress on heroku deployment

--- database/seeds/002-saved_recipes.js
+++ database/seeds/002-saved_recipes.js
@@ -1,12 +1,12 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('saved_recipes').truncate()
+  return knex('saved_recipes')
     .then(function () {
       // Inserts seed entries
       return knex('saved_recipes').insert([
-        {id: 1, recipe_name: 'Garlicky Pasta with Swiss Chard and Beans', recipe_id: 482788, user_id: 2},
-        {id: 2, recipe_name: 'Garlicky Pasta with Swiss Chard and Beans', recipe_id: 482788, user_id: 1},
+        {id: 1, recipe_name: 'Garlicky Pasta with Swiss Chard and Beans', recipe_id: 482788, user_id: 1},
+        {id: 2, recipe_name: 'Garlicky Pasta with Swiss Chard and Beans', recipe_id: 482788, user_id: 2},
         {id: 3, recipe_name: 'Salmon with Sesame-Ginger Quinoa', recipe_id: 755082, user_id: 1},
       ]);
     });
