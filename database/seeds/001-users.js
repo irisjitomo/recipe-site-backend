c5d133d19acbd5c2764a95207e6765dbf52c5f12
unknown <irisjitomo@yahoo.com>
2020-06-14 20:23:13 -0400

    fixed server

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -6,8 +6,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, username: 'Iris', password: bcrypt.hashSync('password', 7)},
-        {id: 2, username: 'Bri', password: bcrypt.hashSync('password', 7)},
+        {username: 'Iris', password: bcrypt.hashSync('password', 7)},
+        {username: 'Bri', password: bcrypt.hashSync('password', 7)},
       ]);
     });
 };
--- database/seeds/002-saved_recipes.js
+++ database/seeds/002-saved_recipes.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('saved_recipes').insert([
-        {id: 1, recipe_name: 'Garlicky Pasta with Swiss Chard and Beans', recipe_id: 482788, user_id: 1},
-        {id: 2, recipe_name: 'Garlicky Pasta with Swiss Chard and Beans', recipe_id: 482788, user_id: 2},
-        {id: 3, recipe_name: 'Salmon with Sesame-Ginger Quinoa', recipe_id: 755082, user_id: 1},
+        {recipe_name: 'Garlicky Pasta with Swiss Chard and Beans', recipe_id: 482788, user_id: 1},
+        {recipe_name: 'Garlicky Pasta with Swiss Chard and Beans', recipe_id: 482788, user_id: 2},
+        {recipe_name: 'Salmon with Sesame-Ginger Quinoa', recipe_id: 755082, user_id: 1},
       ]);
     });
 };
